6cd5347ca0097233b1e7ca03fbd008be6f7a7c6f
Adam Sake Arfansyah <adamsake8@gmail.com>
2023-09-08 14:00:46 +0700

    add data dummy

--- config/USER.ts
+++ config/USER.ts
@@ -11,8 +11,7 @@ const USER = [
       followers: 110000,
       following: 1000,
       profile_img: {
-        background:
-          "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+        background: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
         user: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
       },
       article: [
@@ -23,8 +22,7 @@ const USER = [
           topic: ["Programming", "World"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -46,8 +44,7 @@ const USER = [
       followers: 800,
       following: 100,
       profile_img: {
-        background:
-          "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+        background: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
         user: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
       },
       article: [
@@ -58,8 +55,7 @@ const USER = [
           topic: ["Relationship", "Software Development"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -71,8 +67,7 @@ const USER = [
           topic: ["Relationship", "Love", "Web Development", "Frameworks"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -84,8 +79,7 @@ const USER = [
           topic: ["Relationship", "Love", "Web Development", "Frameworks"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -97,8 +91,7 @@ const USER = [
           topic: ["Relationship", "Love", "Web Development", "Frameworks"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -120,8 +113,7 @@ const USER = [
       followers: 10000,
       following: 1000,
       profile_img: {
-        background:
-          "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+        background: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
         user: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
       },
       article: [
@@ -132,8 +124,7 @@ const USER = [
           topic: ["World", "Relationship"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -155,8 +146,7 @@ const USER = [
       followers: 1200,
       following: 1000,
       profile_img: {
-        background:
-          "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+        background: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
         user: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
       },
       article: [
@@ -167,8 +157,7 @@ const USER = [
           topic: ["Relationship", "News"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
@@ -180,8 +169,52 @@ const USER = [
           topic: ["Self Improvement", "Software Development"],
           createdAt: "20 June 2023",
           img: {
-            banner:
-              "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+          },
+        },
+      ],
+      lists: [],
+      books: [],
+    },
+  },
+  {
+    id: 5,
+    username: "@rianiprameswari",
+    password: "1234",
+    data: {
+      name: "Riani Prameswari",
+      email: "rianiprameswari@mail.com",
+      bio: "Hello my name is Riani Prameswari, thank you so much!",
+      about: `about panjaaanng`,
+      followers: 1200,
+      following: 1000,
+      profile_img: {
+        background: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+        user: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+      },
+      article: [
+        {
+          id: 102345,
+          title: "Resmi IPO, VKTR Andalkan Bisnis Suplai Kendaraan Listrik",
+          articleDescription: `PT VKTR Teknologi Mobilitas Tbk. (VKTR)–perusahaan yang bergerak di sektor perdagangan kendaraan listrik berbasis baterai–memilih model bisnis ke pemerintah (B2G) dan emiten lainnya (B2B) sebagai roda bisnis utamanya. Perusahaan tersebut kini tengah menyasar segmen B2G dengan menyuplai permintaan kendaraan listrik, salah satunya adalah bus listrik. Bisnis suplai bus listrik ini dianggap menjanjikan mengingat operator moda transportasi besar seperti TransJakarta sebelumnya berencana bakal mengoperasikan 10.000 bus listrik hingga 2030 mendatang. Tahun 2022 lalu, anak perusahaan grup Bakrie tersebut sebelumnya telah menandatangani nota kesepahaman dengan TransJakarta untuk mengubah sekitar 3.000 unit bus diesel menjadi listrik (retrofit). Hingga akhir Mei 2023, Kontan melaporkan pihak VKTR telah memasok lebih dari 52 unit bus kepada TransJakarta. “Kita ingin kontribusi di Transjakarta lebih banyak lagi, karena Transjakarta menargetkan 10 ribu bus pada akhir tahun 2030. Jadi semaksimal mungkin kita bisa berkontribusi, kita usahakan,” kata Direktur Utama VKTR Gilarsi W. Setijono sebagaimana dikutip dari Viva.`,
+          topic: ["Relationship", "News"],
+          createdAt: "20 June 2023",
+          img: {
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+            img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
+          },
+        },
+        {
+          id: 99999,
+          title: "Resmi IPO, VKTR Andalkan Bisnis Suplai Kendaraan Listrik",
+          articleDescription: `PT VKTR Teknologi Mobilitas Tbk. (VKTR)–perusahaan yang bergerak di sektor perdagangan kendaraan listrik berbasis baterai–memilih model bisnis ke pemerintah (B2G) dan emiten lainnya (B2B) sebagai roda bisnis utamanya. Perusahaan tersebut kini tengah menyasar segmen B2G dengan menyuplai permintaan kendaraan listrik, salah satunya adalah bus listrik. Bisnis suplai bus listrik ini dianggap menjanjikan mengingat operator moda transportasi besar seperti TransJakarta sebelumnya berencana bakal mengoperasikan 10.000 bus listrik hingga 2030 mendatang. Tahun 2022 lalu, anak perusahaan grup Bakrie tersebut sebelumnya telah menandatangani nota kesepahaman dengan TransJakarta untuk mengubah sekitar 3.000 unit bus diesel menjadi listrik (retrofit). Hingga akhir Mei 2023, Kontan melaporkan pihak VKTR telah memasok lebih dari 52 unit bus kepada TransJakarta. “Kita ingin kontribusi di Transjakarta lebih banyak lagi, karena Transjakarta menargetkan 10 ribu bus pada akhir tahun 2030. Jadi semaksimal mungkin kita bisa berkontribusi, kita usahakan,” kata Direktur Utama VKTR Gilarsi W. Setijono sebagaimana dikutip dari Viva.`,
+          topic: ["Self Improvement", "Software Development"],
+          createdAt: "20 June 2023",
+          img: {
+            banner: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img1: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
             img2: "https://raw.githubusercontent.com/Damskuuii/image/main/esteh.jpeg",
           },
